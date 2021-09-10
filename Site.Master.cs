13166dc1d3d0cb2821cd879d4f636beab1c99238
SiMncube <mncubepraiseworth@gmail.com>
2021-09-10 20:31:22 +0200

    Redirect to Login and Register

--- Site.Master.cs
+++ Site.Master.cs
@@ -13,5 +13,18 @@ namespace M4_major_project
         {
 
         }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Page.ClientScript.RegisterStartupScript(
+            this.GetType(), "OpenWindow", "window.open('https://localhost:44301/Login.aspx','_newtab');", true);
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Page.ClientScript.RegisterStartupScript(
+            this.GetType(), "OpenWindow", "window.open('https://localhost:44301/Register.aspx','_newtab');", true);
+        }
     }
 }
--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -15,6 +15,24 @@ namespace M4_major_project
     {
 
         /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
