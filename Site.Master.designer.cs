cdd2c296e97b34228f977ce16815cea64062a859
SiMncube <mncubepraiseworth@gmail.com>
2021-09-10 21:09:35 +0200

    Small update

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -15,22 +15,22 @@ namespace M4_major_project
     {
 
         /// <summary>
-        /// Button1 control.
+        /// Button3 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Button Button3;
 
         /// <summary>
-        /// Button2 control.
+        /// Button4 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
+        protected global::System.Web.UI.WebControls.Button Button4;
 
         /// <summary>
         /// MainContent control.
