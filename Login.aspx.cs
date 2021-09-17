517eaacb7ef2d0f8b6c0a4207f91cd8395da2b70
SiMncube <mncubepraiseworth@gmail.com>
2021-09-17 14:36:18 +0200

    Login functionality added

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -16,7 +18,43 @@ namespace M4_major_project
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Response.Write("You have successfully logged in");
+            SqlConnection conn = new SqlConnection("Server=146.230.177.46\\ist3;database=group7;User ID=group7;Password=6d52h");
+            conn.Open();
+            string checkUser = "select count(*) from Customer where emailID='" + TextBox3.Text + "'";
+            SqlCommand com = new SqlCommand(checkUser, conn);
+            int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+            conn.Close();
+            if (temp == 1)
+            {
+                conn.Open();
+                string checkPassword = "select password from Customer where password='" + TextBox2.Text + "'";
+                SqlCommand passwordCom = new SqlCommand(checkPassword, conn);
+                string password = "";
+                if (passwordCom.ExecuteScalar() == null)
+                {
+                    Response.Redirect("/Default");
+                    Response.Write("Your login was unsuccessful");
+                    
+                }
+                else
+                {
+                    password = passwordCom.ExecuteScalar().ToString();
+                }
+                if (password.Equals(TextBox2.Text))
+                {
+                    Response.Redirect("/Default");
+                    Session["New"] = TextBox3.Text;
+                    Response.Write("Your login was successful");
+
+                }
+
+
+            }
+            else
+            {
+                Response.Write("Your login was unsuccessful");
+            }
+            
         }
     }
 }
