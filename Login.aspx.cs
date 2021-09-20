e043fb7844c0a883a8f9b264c568e88456167884
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-09-20 13:46:42 +0200

    login redesign

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -17,51 +17,7 @@ namespace M4_major_project
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            SqlConnection conn = new SqlConnection("Server=146.230.177.46\\ist3;database=group7;User ID=group7;Password=6d52h");
-            conn.Open();
-            string checkUser = "select count(*) from Customer where emailID='" + TextBox1.Text + "'";
-            SqlCommand com = new SqlCommand(checkUser, conn);
-            int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
-            conn.Close();
-            if (temp == 1)
-            {
-                conn.Open();
-                string checkPassword = "select password from Customer where password='" + TextBox2.Text + "'";
-                SqlCommand passwordCom = new SqlCommand(checkPassword, conn);
-                string password = "";
-                if (passwordCom.ExecuteScalar() == null)
-                {
-                    Response.Write("Your login was unsuccessful");
-                    Label2.Visible = true;
-                    
-                }
-                else
-                {
-                    password = passwordCom.ExecuteScalar().ToString();
-                }
-                if (password.Equals(TextBox2.Text))
-                {
-                    check++;
-                    Response.Redirect("/Default");
-                    
-                   // Session["New"] = TextBox1.Text;
-                  //  Response.Write("Your login was successful");
-                    
-
-                }
-
-
-            }
-            else
-            {
-                Response.Write("Your login was unsuccessful");
-                Label2.Visible = true;
-            }
-            
+        
         }
     }
 }
