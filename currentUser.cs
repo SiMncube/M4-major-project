409ac0e973497efb939c425df7ab68db3278ad8d
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-09-21 00:27:18 +0200

    ..

---
+++ currentUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class currentUser
+{
+        private static string emailID;
+        private static bool isAdmin = false;
+        public static string getEmailID()
+        {
+            return emailID;
+        }
+        public static void setEmailID(string e)
+        {
+            emailID = e;
+        }
+}
