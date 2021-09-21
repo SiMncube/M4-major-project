8ade61a7137c8aa524ab993d20a142dfaffe5ff1
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-09-21 15:00:13 +0200

    ...

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace M4_major_project
@@ -11,7 +13,7 @@ namespace M4_major_project
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            HtmlAnchor btn1 = (HtmlAnchor)FindControl("setUser");
         }
     }
 }
