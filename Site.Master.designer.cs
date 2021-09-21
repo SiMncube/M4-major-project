e030a66cd1487f8efcb22f756c1386395681b6ca
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-09-21 13:12:18 +0200

    Loggin and register to the right

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -69,6 +69,24 @@ namespace M4_major_project
         protected global::System.Web.UI.HtmlControls.HtmlAnchor Register1;
 
         /// <summary>
+        /// A1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor A1;
+
+        /// <summary>
+        /// A2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor A2;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
