c4388dc4053f48a112976cba4a0e440929194267
SihleNxasane <sinxasane@gmail.com>
2021-10-24 14:10:54 +0200

    viewBooking link in footer

---
+++ Database.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M4_major_project
+{
+    public static class Database
+    {
+        public static FullDataSet ds = new FullDataSet();
+    }
+}
