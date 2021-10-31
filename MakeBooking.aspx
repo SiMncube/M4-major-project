37af20e2ddc646cc9cf3163a70b337284ee8dc95
SihleNxasane <sinxasane@gmail.com>
2021-10-31 13:47:39 +0200

    Make Booking.

--- MakeBooking.aspx
+++ MakeBooking.aspx
@@ -4,74 +4,74 @@
     <div>
         <table style="width: 100%;">
             <tr>
-                <td style="width: 428px">&nbsp;</td>
-                <td style="width: 309px" class="modal-sm">&nbsp;</td>
+                <td style="width: 467px">&nbsp;</td>
+                <td style="width: 306px" class="modal-sm">&nbsp;</td>
                 <td>&nbsp;</td>
             </tr>
             <tr>
-                <td style="width: 428px">&nbsp;</td>
-                <td style="width: 309px" class="modal-sm">
+                <td style="width: 467px">&nbsp;</td>
+                <td style="width: 306px" class="modal-sm">
                     <div class="form-group">
                         <asp:Label ID="Label3" runat="server" Text="Check-In Date:"></asp:Label>
-                        <asp:TextBox ID="TextBox3" runat="server" Style="margin-right: 29" TextMode="DateTime" Width="309px"></asp:TextBox>
+                        <asp:TextBox ID="TextBox3" runat="server" Style="margin-right: 29" TextMode="DateTime" Width="300px"></asp:TextBox>
                     </div>
                 </td>
                 <td>&nbsp;</td>
             </tr>
             <tr>
-                <td style="width: 428px; height: 37px;"></td>
-                <td style="width: 309px; height: 37px;">
+                <td style="width: 467px; height: 37px;"></td>
+                <td style="width: 306px; height: 37px;">
                     <div class="form-group">
                         <asp:Label ID="Label2" runat="server" Text="Check-Out Date:"></asp:Label>
-                        <asp:TextBox ID="TextBox2" runat="server" TextMode="DateTime" Width="311px"></asp:TextBox>
+                        <asp:TextBox ID="TextBox2" runat="server" TextMode="DateTime" Width="300px"></asp:TextBox>
                     </div>
                 </td>
                 <td style="height: 37px"></td>
             </tr>
             <tr>
-                <td style="width: 428px">&nbsp;</td>
-                <td style="width: 309px" class="modal-sm">
+                <td style="width: 467px">&nbsp;</td>
+                <td style="width: 306px" class="modal-sm">
                     <div class="form-group">
                         <label for="subject">Single rooms:</label>
-                        <select id="singleBox" name="subject" class="form-control" required="required" runat="server">
+                        <select id="singleBox" name="subject" class="form-control" required="required" runat="server" style="width: 100%">
                         </select>
                     </div>
                 </td>
                 <td>&nbsp;</td>
             </tr>
             <tr>
-                <td style="width: 428px">&nbsp;</td>
-                <td style="width: 309px" class="modal-sm">
+                <td style="width: 467px">&nbsp;</td>
+                <td style="width: 306px" class="modal-sm">
                     <div class="form-group">
                         <label for="subject">Double Rooms:</label>
-                        <select id="doubleBox" name="subject" class="form-control" required="required" runat="server">
+                        <select id="doubleBox" name="subject" class="form-control" required="required" runat="server" style="width: 100%">
                         </select>
                     </div>
                 </td>
                 <td>&nbsp;</td>
             </tr>
             <tr>
-                <td style="width: 428px">&nbsp;</td>
-                <td style="width: 309px" class="modal-sm">
+                <td style="width: 467px">&nbsp;</td>
+                <td style="width: 306px" class="modal-sm">
                     <div class="form-group">
                         <asp:Label ID="Label1" runat="server" Text="Amount Due: "></asp:Label>
-                        <asp:TextBox ID="TextBox1" runat="server" Width="307px"></asp:TextBox>
+                        <asp:TextBox ID="TextBox1" runat="server" Width="300px"></asp:TextBox>
                     </div>
                 </td>
                 <td>&nbsp;</td>
             </tr>
             <tr>
-                <td style="width: 428px; height: 25px;"></td>
-                <td style="width: 309px; height: 25px;">
+                <td style="width: 467px; height: 25px;"></td>
+                <td style="width: 306px; height: 25px;">
                     <div class="form-group">
-                        <asp:Button ID="saveBtn" runat="server" Text="Save Booking" Width="318px" />
+                        <asp:Button ID="saveBtn" runat="server" Text="Save Booking" Width="300px" />
                     </div>
                 </td>
                 <td style="height: 25px"></td>
             </tr>
             <tr>
-                <td style="width: 428px">&nbsp;</td>
-                <td style="width: 309px" class="modal-sm">&nbsp;</td>
+                <td style="width: 467px">&nbsp;</td>
+                <td style="width: 306px" class="modal-sm">&nbsp;</td>
                 <td>&nbsp;</td>
             </tr>
         </table>
--- MakeBooking.aspx.cs
+++ MakeBooking.aspx.cs
@@ -21,5 +21,363 @@ namespace M4_major_project
                 doubleBox.Items.Add(i + " ");
             }
         }
+
+        /*
+    string currentCustomerEmailID;
+    DateTime dateIn = DateTime.Today;
+    DateTime dateOut = DateTime.Today;
+    int numberOfNights;
+    string bookingMethod = "Admin";
+    string bookingStatus = "inComplete";
+    double amountDue = 0;
+
+    ArrayList availableSingleRooms = new ArrayList();
+    ArrayList availableDoubleRooms = new ArrayList();
+
+    int numberOfSingleRooms = 0;
+    int numberOfDoubleRooms = 0;
+
+
+    private bool bookingIsComplete(string summaryID)
+    {
+        for (int i = 0; i < fullDs.BookingSummary.Rows.Count; i++)
+        {
+            if ((fullDs.Tables["BookingSummary"].Rows[i]["summaryID"].ToString().Equals(summaryID)) &&
+               (fullDs.Tables["BookingSummary"].Rows[i]["bookingStatus"].ToString().Equals("Complete")))
+                return true;
+        }
+        return false;
+    }
+
+    private bool isRoomAvailable(int roomID, DateTime dateID)
+    {
+        for (int i = 0; i < fullDs.BookedRoom.Rows.Count; i++)
+        {
+            if (((fullDs.Tables["BookedRoom"].Rows[i]["dateID"].ToString().Equals(dateID.ToString())) &&
+               (int.Parse(fullDs.Tables["BookedRoom"].Rows[i]["roomID"].ToString()) == roomID)) &&
+               (bookingIsComplete(fullDs.Tables["BookedRoom"].Rows[i]["summaryID"].ToString())))
+            //if the record of the room in bookedRoom is complete then that room is not available
+            //however if the booking of that room has been cancelled than that room has to be marked available
+            //even though it exist in bookedRoom records
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void updateAvailableRoomList()
+    {
+        availableSingleRooms = new ArrayList();
+        availableDoubleRooms = new ArrayList();
+
+        for (int roomID = 1; roomID <= 15; roomID++)
+        {
+            bool roomAvailableFlag = true;
+            for (DateTime dateID = dateIn; DateTime.Compare(dateID, dateOut) < 0; dateID = dateID.AddDays(1))
+            {
+                if (!isRoomAvailable(roomID, dateID))
+                {
+                    roomAvailableFlag = false;
+                    break;
+                }
+            }
+
+            if (roomAvailableFlag)
+            {
+                if (roomID <= 7)
+                    availableSingleRooms.Add(roomID);
+                else
+                    availableDoubleRooms.Add(roomID);
+            }
+        }
+    }
+
+    private string getAmountDue(ComboBox cb1, ComboBox cb2)
+    {
+        double amountDueForSingleRooms;
+        double amountDueForDoubleRooms;
+        numberOfNights = dateOut.Subtract(dateIn).Days;
+        if (numberOfNights == 0)
+            numberOfNights++;
+
+        if (cb1.SelectedItem == null || cb1.SelectedItem.ToString() == "0")
+        {
+            amountDueForSingleRooms = 0.0;
+        }
+        else
+        {
+            numberOfSingleRooms = int.Parse(cb1.SelectedItem.ToString());
+            amountDueForSingleRooms = (numberOfSingleRooms * 450 * numberOfNights);
+        }
+        if (cb2.SelectedItem == null || cb2.SelectedItem.ToString() == "0")
+        {
+            amountDueForDoubleRooms = 0.0;
+        }
+        else
+        {
+            numberOfDoubleRooms = int.Parse(cb2.SelectedItem.ToString());
+            amountDueForDoubleRooms = (numberOfDoubleRooms * 800 * numberOfNights);
+        }
+        amountDue = amountDueForSingleRooms + amountDueForDoubleRooms;
+        return "R " + amountDue.ToString() + ".00";
+    }
+
+    private void loadAvailableSinlges(ComboBox cb)
+    {
+
+        cb.Items.Clear();
+        cb.Items.Add("0");
+        for (int i = 0; i < availableSingleRooms.Count; i++)
+            cb.Items.Add(i + 1 + "");
+    }
+
+    private void loadAvailableDoubles(ComboBox cb)
+    {
+        cb.Items.Clear();
+        cb.Items.Add("0");
+        for (int i = 0; i < availableDoubleRooms.Count; i++)
+            cb.Items.Add(i + 1 + "");
+    }
+
+    private bool dateIsValid()
+    {
+        if (((DateTime.Compare(DateTime.Today, dateIn) <= 0) && (DateTime.Compare(DateTime.Today, dateOut) < 0) && (DateTime.Compare(dateIn, dateOut) < 0)))
+            return true;
+        return false;
+    }
+
+    public string arrayToString(int[] array)
+    {
+        string s = "Room: ";
+
+        if (array.Length == 0)
+            return "none";
+        else
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                s += array[i];
+                if (i != array.Length - 1)
+                    s += ", ";
+            }
+        }
+        return s;
+    }
+
+    private void updateBookingSummary(string callAmountDueMethod)
+    {
+        int[] singleAllocatedRooms = new int[numberOfSingleRooms];
+        int[] doubleAllocatedRooms = new int[numberOfDoubleRooms];
+
+        bookingSummaryTa.Insert(currentCustomerEmailID, dateIn, dateOut, numberOfNights, bookingMethod, bookingStatus, callAmountDueMethod);
+        int summaryID = (int)bookingSummaryTa.getLastRecord();
+        currentBooking.setSummaryID(summaryID);
+
+        for (int i = 0; i < numberOfSingleRooms; i++) //adding single rooms to bookedRoom table
+        {
+            for (DateTime dateID = dateIn; DateTime.Compare(dateID, dateOut) < 0; dateID = dateID.AddDays(1))
+            {
+                singleAllocatedRooms[i] = (int)availableSingleRooms[i];
+            }
+        }
+
+        for (int i = 0; i < numberOfDoubleRooms; i++) //adding double rooms to bookedRoom table
+        {
+            for (DateTime dateID = dateIn; DateTime.Compare(dateID, dateOut) < 0; dateID = dateID.AddDays(1)) //adding double rooms to bookedRoom table
+            {
+                doubleAllocatedRooms[i] = (int)availableDoubleRooms[i];
+            }
+        }
+
+        int[] allAllocatedRooms = new int[singleAllocatedRooms.Length + doubleAllocatedRooms.Length];
+        Array.Copy(singleAllocatedRooms, allAllocatedRooms, singleAllocatedRooms.Length);
+        Array.Copy(doubleAllocatedRooms, 0, allAllocatedRooms, singleAllocatedRooms.Length, doubleAllocatedRooms.Length);
+        currentBooking.setRoomIDs(allAllocatedRooms);
+
+        this.bookingSummaryTa.Update(this.fullDs.BookingSummary);
+        this.bookingSummaryTa.Fill(this.fullDs.BookingSummary);
+
+        //These initailizes the invoice fields
+        for (int i = 0; i < fullDs.Customer.Rows.Count; i++)
+        {
+            if (fullDs.Customer[i].emailID.Equals(currentCustomerEmailID))
+            {
+                Email.customerName = fullDs.Customer[i].name;
+                Email.customerSurname = fullDs.Customer[i].surname;
+                Email.customerIdNumber = fullDs.Customer[i].idNumber;
+            }
+        }
+        Email.customerEmail = currentCustomerEmailID;
+        Email.bookingID = summaryID.ToString();
+        Email.bookingStatus = bookingStatus;
+        Email.bookingMethod = bookingMethod;
+        Email.dateIn = dateIn.ToString("dd/MM/yyyy") + " 12:00 PM";
+        Email.dateOut = dateOut.ToString("dd/MM/yyyy") + " 12:00 PM";
+        Email.numberOfNights = numberOfNights.ToString();
+        Email.numberOfSingles = singleAllocatedRooms.Length.ToString();
+        Email.numberOfDoubles = doubleAllocatedRooms.Length.ToString();
+        Email.singleRoomIDs = arrayToString(singleAllocatedRooms);
+        Email.doubleRoomIDs = arrayToString(doubleAllocatedRooms);
+        Email.amountDue = callAmountDueMethod;
+
+    }
+
+    private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+    {
+        dateIn = dateTimePicker1.Value.Date;
+        dateOut = dateTimePicker2.Value.Date;
+        if (dateIsValid())
+        {
+            label24.Visible = false;
+            comboBox1.Enabled = true;
+            comboBox2.Enabled = true;
+            updateAvailableRoomList();
+            loadAvailableSinlges(this.comboBox1);
+            loadAvailableDoubles(this.comboBox2);
+        }
+        else
+        {
+            label24.Visible = true;
+            comboBox1.Enabled = false;
+            comboBox2.Enabled = false;
+        }
+    }
+
+    private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+    {
+        dateIn = dateTimePicker1.Value.Date;
+        dateOut = dateTimePicker2.Value.Date;
+        label24.Visible = false;
+        if (dateIsValid())
+        {
+            label24.Visible = false;
+            comboBox1.Enabled = true;
+            comboBox2.Enabled = true;
+            updateAvailableRoomList();
+            loadAvailableSinlges(this.comboBox1);
+            loadAvailableDoubles(this.comboBox2);
+        }
+        else
+        {
+            label24.Visible = true;
+            comboBox1.Enabled = false;
+            comboBox2.Enabled = false;
+        }
+    }
+
+    private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        textBox5.Text = getAmountDue(comboBox1, comboBox2);
+        numberOfSingleRooms = int.Parse(comboBox1.SelectedItem.ToString());
+        if (amountDue != 0)
+            button8.Enabled = true;
+        else
+            button8.Enabled = false;
+    }
+
+    private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        textBox5.Text = getAmountDue(comboBox1, comboBox2);
+        numberOfDoubleRooms = int.Parse(comboBox2.SelectedItem.ToString());
+        if (amountDue != 0)
+            button8.Enabled = true;
+        else
+            button8.Enabled = false;
+    }
+
+    private void button8_Click(object sender, EventArgs e)
+    {
+        updateBookingSummary(getAmountDue(comboBox1, comboBox2).ToString());
+        comboBox1.Enabled = false;
+        comboBox2.Enabled = false;
+        button8.Enabled = false;
+        dateTimePicker1.Enabled = false;
+        dateTimePicker2.Enabled = false;
+        this.bookingSummaryTa.Update(this.fullDs.BookingSummary);
+        this.bookingSummaryTa.Fill(this.fullDs.BookingSummary);
+        bookedRoomTa.Update(fullDs.BookedRoom);
+        bookedRoomTa.Fill(fullDs.BookedRoom);
+
+        currentUser.setEmailID(currentCustomerEmailID);
+        ConfirmbookingForm c = new ConfirmbookingForm();
+        c.ShowDialog();
+    }
+
+    private void tabPage2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void tabPage3_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void textBox3_TextChanged(object sender, EventArgs e)
+    {
+        customerTa.FillByAnythingGiven(fullDs.Customer, textBox3.Text);
+        customerDataGridView.ClearSelection();
+        panel5.Enabled = false;
+        label33.Visible = false;
+
+    }
+
+    private void button10_Click(object sender, EventArgs e)
+    {
+        panel5.Enabled = true;
+        dateTimePicker1.Enabled = true;
+        dateTimePicker2.Enabled = true;
+
+        //pre populate the fields with booking details we have
+    }
+
+    private void tabPage6_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void groupBox1_Enter(object sender, EventArgs e)
+    {
+
+    }
+
+    private void tabPage4_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void customerDataGridView_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+    {
+        currentCustomerEmailID = customerDataGridView.CurrentRow.Cells[0].Value.ToString();
+        label33.Text = "To Process Booking for: " + customerDataGridView.CurrentRow.Cells[1].Value.ToString() + " " + customerDataGridView.CurrentRow.Cells[2].Value.ToString() + " (" + customerDataGridView.CurrentRow.Cells[0].Value.ToString() + ")";
+        label33.Visible = true;
+        button10.Enabled = true;
+    }
+    private void button12_Click(object sender, EventArgs e)
+    {
+        customerDataGridView.ClearSelection();
+
+        textBox3.Clear();
+        textBox5.Clear();
+        label24.Visible = false;
+        label33.Visible = false;
+        panel5.Enabled = false;
+        button10.Enabled = false;
+
+        dateTimePicker1.Value = DateTime.Today;
+        dateTimePicker2.Value = DateTime.Today;
+
+        bookingSummaryTa.Update(fullDs.BookingSummary);
+        bookingSummaryTa.Fill(fullDs.BookingSummary);
+        bookedRoomTa.Update(fullDs.BookedRoom);
+        bookedRoomTa.Fill(fullDs.BookedRoom);
+    }
+
+    private void label20_Click(object sender, EventArgs e)
+    {
+
+    }
+         */
     }
 }
