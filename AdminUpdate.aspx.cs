734801eb45249d6e78f189d6571af1fa71efafa7
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-11-12 23:43:47 +0200

    ...

--- AdminUpdate.aspx.cs
+++ AdminUpdate.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -16,7 +17,28 @@ namespace M4_major_project
 
         protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //nameTextBox.Text 
+            FullDataSet fullDs = new FullDataSet();
+            FullDataSetTableAdapters.CustomerTableAdapter taCustomer = new FullDataSetTableAdapters.CustomerTableAdapter();
+            taCustomer.Fill(fullDs.Customer);
+            DataRow dataRow = fullDs.Customer.NewRow();
+            for (int i = 0; i < dataRow.ItemArray.Length; i++)
+            {
+                dataRow[i] = GridView1.SelectedRow.Cells[i].Text;
+            }
+            updateTextBox(emailTextBox, dataRow[1].ToString());
+            updateTextBox(confirmEmailTextBox, dataRow[1].ToString());
+            updateTextBox(nameTextBox, dataRow[3].ToString());
+            updateTextBox(surnameTextBox, dataRow[2].ToString());
+            updateTextBox(idTextBox, dataRow[4].ToString());
+            updateTextBox(cellTextBox, dataRow[5].ToString());
+            updateTextBox(streetTextBox, dataRow[6].ToString());
+            updateTextBox(surbubNameTextBox, dataRow[7].ToString());
+            updateTextBox(cityTextBox, dataRow[8].ToString());
+            updateTextBox(postalTextBox, dataRow[9].ToString());
+        }
+        private void updateTextBox(TextBox t , string s)
+        {
+            t.Text = s;
         }
     }
 }
