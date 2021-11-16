aa67f70031b270706ac6f23a578c1276948b4220
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-11-16 02:40:03 +0200

    .....

---
+++ CurrentReset.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M4_major_project
+{
+    public class CurrentReset
+    {
+        private static string emailID;
+        private static string otpString;
+        public static void setEmailID(string e)
+        {
+            emailID = e;
+        }
+
+        public static string getEmailID()
+        {
+            return emailID;
+        }
+        public static void setOtpString(string e)
+        {
+            otpString = e;
+        }
+
+        public static string getOtpString()
+        {
+            return otpString;
+        }
+    }
+}
