25e9f022acadec29ba0ff5b2eef536b9ede368eb
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-11-16 15:27:54 +0200

    Reset password done

--- ResetPasswordOTP.aspx.cs
+++ ResetPasswordOTP.aspx.cs
@@ -12,9 +12,7 @@ namespace M4_major_project
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            modalBody.InnerHtml = "<p>You have successfully registered, You can now make bookings.</p>";
-            ScriptManager.RegisterStartupScript(this, this.GetType(), "Pop", "$('#myModal').modal('show');", true);
-            Context.Response.Write("<script type='text/javascript'>window.frameElement.navigateParent('/Default');</script>");
+
         }
 
         protected void confirmBtn_Click(object sender, EventArgs e)
@@ -34,7 +32,7 @@ namespace M4_major_project
         }
         protected void closeBtn_Click(object sender, EventArgs e)
         {
-            Response.Redirect("/Default");
+            Response.Redirect("/ChangePassword");
         }
     }
 }
