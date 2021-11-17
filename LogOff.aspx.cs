5a4ad01d867a2d836a7f544c51029037c4b398f6
Kgaugelo11 <65261366+Kgaugelo11@users.noreply.github.com>
2021-11-17 13:45:52 +0200

    Log off functionality done

---
+++ LogOff.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace M4_major_project
+{
+    public partial class LogOff : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Login.check = 0;
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Login");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Login");
+        }
+    }
+}
